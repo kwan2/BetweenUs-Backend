d697fce558b892484765fc81fb19c42c1aedac3d
parkikbum <pibum@naver.com>
2022-11-23 18:17:21 +0900

    init: add dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(new ValidationPipe()).useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+  await app.listen(process.env.PORT_NUMBER);
 }
 bootstrap();
