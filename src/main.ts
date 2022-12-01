9611d4ae304f53fbf4cd6a5ee9e696353bbbff09
kwan2 <jgh9987@naver.com>
2022-12-02 00:36:27 +0900

    [Modify] #22 cors 설정 -> main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe()).useGlobalPipes(
     new ValidationPipe({
       transform: true,
