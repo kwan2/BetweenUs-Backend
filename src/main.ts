0faa1ca4ad587fb0395a4c4570865baf43f03126
kwan2 <jgh9987@naver.com>
2022-12-02 20:44:54 +0900

    [modify] #22 enablecors config

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: true, // true면 아무나 접근이 가능하도록 허용한다. 배포단계에서는 url을 적어서 하나의 도메인에서만 백엔드 애플리케이션에 접근할 수 있도록 한다.
+    credentials: true, // 신뢰가능한 것인지 체크해준다.
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  });
   app.useGlobalPipes(new ValidationPipe()).useGlobalPipes(
     new ValidationPipe({
       transform: true,
