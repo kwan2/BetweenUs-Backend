e666b372cccc69198120b760a221693e4587a697
parkikbum <pibum@naver.com>
2022-12-07 11:33:12 +0900

    feat: fix guide line

--- src/guideline/guideline.controller.ts
+++ src/guideline/guideline.controller.ts
@@ -1,40 +1,52 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { ResponseBuilder, ResponseDto } from 'src/common/dto/response.dto';
 import { Public } from 'src/config/skip-auth.decorator';
 import { GuidelineService } from './guideline.service';
 
 @Controller('guideline')
 export class GuidelineController {
-    constructor(
-        private readonly guidelineService : GuidelineService,
-    ) {}
-    
-    @Public()
-    @HttpCode(HttpStatus.CREATED)
-    @Put(':id')
-    async createGuideline(@Param('id') space_id : number, @Body() codeconvention : string) : Promise<ResponseDto<string>> {  
-        const guideline = await this.guidelineService.createGuideline(codeconvention,space_id);
-        return new ResponseBuilder<string>()
-            .status(HttpStatus.CREATED)
-            .message('create Guideline successfully')
-            .body(guideline)
-            .build();
-    }
-    /**
-     * 
-     * @param id : user_id -> space_id 로 수정 할 것임. 
-     * @returns 
-     */
-    @Public()
-    @HttpCode(HttpStatus.OK)
-    @Get('get/:id')
-    async getAllguideline (@Param('id') id : number ) : Promise<ResponseDto<any>> {
-        const guideline = this.guidelineService.getByspacdID(id);
-        return new ResponseBuilder<any>()
-            .status(HttpStatus.OK)
-            .message('모든 가이드라인 출력 완료')
-            .body(guideline)
-            .build();
-    }
-    
+  constructor(private readonly guidelineService: GuidelineService) {}
+
+  @Public()
+  @HttpCode(HttpStatus.CREATED)
+  @Put(':id')
+  async createGuideline(
+    @Param('id') space_id: number,
+    @Body('codeconvention') codeconvention: string,
+  ): Promise<ResponseDto<string>> {
+    const guideline = await this.guidelineService.createGuideline(
+      codeconvention,
+      space_id,
+    );
+    return new ResponseBuilder<string>()
+      .status(HttpStatus.CREATED)
+      .message('create Guideline successfully')
+      .body(guideline)
+      .build();
+  }
+  /**
+   *
+   * @param id : user_id -> space_id 로 수정 할 것임.
+   * @returns
+   */
+  @Public()
+  @HttpCode(HttpStatus.OK)
+  @Get('get/:id')
+  async getAllguideline(@Param('id') id: number): Promise<ResponseDto<any>> {
+    const guideline = this.guidelineService.getByspacdID(id);
+    return new ResponseBuilder<any>()
+      .status(HttpStatus.OK)
+      .message('모든 가이드라인 출력 완료')
+      .body(guideline)
+      .build();
+  }
 }
--- src/guideline/guideline.service.ts
+++ src/guideline/guideline.service.ts
@@ -6,35 +6,37 @@ import { GuidelineEntity } from './entity/guideline.entity';
 
 @Injectable()
 export class GuidelineService {
+  constructor(
+    @InjectRepository(GuidelineEntity)
+    private readonly guidelineRepository: Repository<GuidelineEntity>,
+  ) {}
 
-    constructor(
-        @InjectRepository(GuidelineEntity)
-        private readonly guidelineRepository : Repository<GuidelineEntity>,
-    ) {}
-
-    async createGuideline(codeconvention : string , space_id : number ) : Promise<string> {
-        const guideline = new GuidelineEntity();
-        guideline.codeconvention = codeconvention;
-        guideline.space_id = space_id;
-
-        await this.guidelineRepository.createQueryBuilder()
-            .insert()
-            .into(GuidelineEntity,['codeconvention','space_id'])
-            .values(guideline)
-            .orUpdate(['codeconvention'])
-            .execute();
-        return codeconvention;
-        
-    }
-    async getByspacdID (space_id : number) : Promise<any> {
-        const codeconvention = await this.guidelineRepository.find({
-            select : {
-                codeconvention : true,
-            },
-            where : {
-                space_id : space_id,
-            }
-        });
-        return codeconvention;
-    }
+  async createGuideline(
+    codeconvention: string,
+    space_id: number,
+  ): Promise<string> {
+    const guideline = new GuidelineEntity();
+    guideline.codeconvention = codeconvention;
+    guideline.space_id = space_id;
+    
+    await this.guidelineRepository
+      .createQueryBuilder()
+      .insert()
+      .into(GuidelineEntity, ['codeconvention', 'space_id'])
+      .values(guideline)
+      .orUpdate(['codeconvention'])
+      .execute();
+    return codeconvention;
+  }
+  async getByspacdID(space_id: number): Promise<any> {
+    const codeconvention = await this.guidelineRepository.find({
+      select: {
+        codeconvention: true,
+      },
+      where: {
+        space_id: space_id,
+      },
+    });
+    return codeconvention;
+  }
 }
